c51db6de7174f4cf5e1a0ec06fa2a17b55507c64
SachinBalagam <balagamsachin337@gmail.com>
2023-07-24 13:37:08 +0000

    First Commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const element = (
+  <div className="bg-container">
+    <h1 className="heading">Super Over League</h1>
+    <div>
+      <img
+        className="picture"
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        alt="RCB"
+      />
+      <img
+        className="picture"
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        alt="CSK"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
